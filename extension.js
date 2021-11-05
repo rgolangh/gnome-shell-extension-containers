5676dd6358976719fc4f4ef70cd21a4f1b8bbd58
Florian Müllner <fmuellner@gnome.org>
2021-11-05 19:06:54 +0100

    containersMenu: Handle refreshing menu internally
    It's slightly odd API to tell the button from the outside when
    to render the menu (in particular when existing menu items aren't
    removed first).

--- extension.js
+++ extension.js
@@ -24,7 +24,6 @@ function enable() {
     Podman.discoverPodmanVersion();
     containersMenu = new ContainersMenu();
     Logger.debug(containersMenu);
-    containersMenu.renderMenu();
     Main.panel.addToStatusArea("containers-menu", containersMenu);
 }
 
@@ -55,15 +54,17 @@ var ContainersMenu = GObject.registerClass(
 
             hbox.add_child(icon);
             this.add_child(hbox);
-            this.connect("button_press_event", () => {
+
+            this.menu.connect("open-state-changed", () => {
                 if (this.menu.isOpen) {
                     this.menu.removeAll();
-                    this.renderMenu();
+                    this._renderMenu();
                 }
             });
+            this._renderMenu();
         }
 
-        renderMenu() {
+        _renderMenu() {
             try {
                 const containers = Podman.getContainers();
                 Logger.info(`found ${containers.length} containers`);
